0bb11a880e2dffee5bff417a0f82562dad824a68
Steven <stevenmai1111@hotmail.com>
2022-07-22 14:58:25 +1000

    Added way to get potion effect

--- src/main/java/dungeonmania/PotionManager.java
+++ src/main/java/dungeonmania/PotionManager.java
@@ -6,19 +6,26 @@ import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class PotionManager {
+    private Potion currPotion = null;
     private Queue<Potion> potionQueue = new LinkedBlockingQueue<>();
     public void addPotionEffect(int duration, Potion potion) {
         for (int i = 0; i < duration; i++) {
             potionQueue.add(potion);
         }
     }
-
+    public Potion getCurrPotion() {
+        return currPotion;
+    }
     /**
      * Gets the next queued potion effect
      * @return null if there are no potion effects else the potion
      */
     public Potion getNextEffect() {
-        if (potionQueue.isEmpty()) return null;
-        return potionQueue.remove();
+        if (potionQueue.isEmpty()) {
+            currPotion = null;
+        } else {
+            currPotion = potionQueue.remove();
+        }
+        return currPotion;
     }
 }
