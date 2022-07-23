606130179601a0b18cf9dfcb3b7646012db6defd
Theo <theoandpebbles@hotmail.com>
2022-07-23 13:13:05 +1000

    fixed issue where you could pick up two keys

--- src/main/java/dungeonmania/Collisions/Collect.java
+++ src/main/java/dungeonmania/Collisions/Collect.java
@@ -4,6 +4,7 @@ import dungeonmania.Entity;
 import dungeonmania.Player;
 import dungeonmania.CollectibleEntities.CollectibleEntity;
 import dungeonmania.CollectibleEntities.InventoryObject;
+import dungeonmania.CollectibleEntities.Key;
 import dungeonmania.util.Direction;
 import java.util.List;
 
@@ -23,8 +24,11 @@ public class Collect extends Collision {
         Player player = (Player) moved;
         CollectibleEntity collectibleEntity = (CollectibleEntity) collided;
         player.setPosition(collectibleEntity.getPosition());
-        player.getInventory().add((InventoryObject) collectibleEntity.getCollectible());
-        entityList.remove(collided);
+        if (!(collectibleEntity.getCollectible() instanceof Key && player.getInventory().stream().anyMatch(x->x instanceof Key))) {
+            player.getInventory().add((InventoryObject) collectibleEntity.getCollectible());
+            entityList.remove(collided);
+        }
+
     }
     
     
