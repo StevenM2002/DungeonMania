3e88b93ce8a524e1c227db43190db64c8123a0d8
Sean Huynh <sean.huynh@student.unsw.edu.au>
2022-07-28 21:37:41 +1000

    Maze

--- src/test/java/dungeonmania/MovingEntitiesTest/TestBattles.java
+++ src/test/java/dungeonmania/MovingEntitiesTest/TestBattles.java
@@ -10,14 +10,32 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController;
+import dungeonmania.CollectibleEntities.Bow;
+import dungeonmania.CollectibleEntities.InventoryObject;
+import dungeonmania.CollectibleEntities.InvincibilityPotion;
+import dungeonmania.CollectibleEntities.MidnightArmor;
+import dungeonmania.CollectibleEntities.Shield;
+import dungeonmania.CollectibleEntities.Sword;
+import dungeonmania.exceptions.InvalidActionException;
 import dungeonmania.response.models.BattleResponse;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.response.models.RoundResponse;
 import dungeonmania.util.Direction;
 
 public class TestBattles {
-    private static DungeonResponse genericSpiderSequence(DungeonManiaController controller, String configFile) {
+    private List<InventoryObject> inventory;
+
+    private void setInventory(List<InventoryObject> inventory) {
+        this.inventory = inventory;
+    }
+
+    private List<InventoryObject> getLocalInventory() {
+        return this.inventory;
+    }
+
+    private DungeonResponse genericSpiderSequence(DungeonManiaController controller, String configFile) {
         DungeonResponse initialResponse = controller.newGame("d_battleTest_basicSpider", configFile);
+        setInventory(controller.getPlayer().getInventory());
         int enemyCount = countEntityOfType(initialResponse, "spider");
 
         assertEquals(1, countEntityOfType(initialResponse, "player"));
@@ -25,8 +43,9 @@ public class TestBattles {
         return controller.tick(Direction.DOWN);
     }
 
-    private static DungeonResponse genericZombieSequence(DungeonManiaController controller, String configFile) {
+    private DungeonResponse genericZombieSequence(DungeonManiaController controller, String configFile) {
         DungeonResponse initialResponse = controller.newGame("d_battleTest_basicZombieToast", configFile);
+        setInventory(controller.getPlayer().getInventory());
         int enemyCount = countEntityOfType(initialResponse, "zombie_toast");
 
         assertEquals(1, countEntityOfType(initialResponse, "player"));
@@ -34,8 +53,9 @@ public class TestBattles {
         return controller.tick(Direction.RIGHT);
     }
 
-    private static DungeonResponse genericHydraSequence(DungeonManiaController controller, String configFile) {
+    private DungeonResponse genericHydraSequence(DungeonManiaController controller, String configFile) {
         DungeonResponse initialResponse = controller.newGame("d_battleTest_basicHydra", configFile);
+        setInventory(controller.getPlayer().getInventory());
         int enemyCount = countEntityOfType(initialResponse, "hydra");
 
         assertEquals(1, countEntityOfType(initialResponse, "player"));
@@ -43,17 +63,39 @@ public class TestBattles {
         return controller.tick(Direction.RIGHT);
     }
 
-    private void assertBattleCalculations(String enemyType, BattleResponse battle, boolean enemyDies,
+    private void assertBattleCalculations(List<InventoryObject> inventory, String enemyType, BattleResponse battle, boolean enemyDies,
             String configFilePath) {
         List<RoundResponse> rounds = battle.getRounds();
         double playerHealth = Double.parseDouble(getValueFromConfigFile("player_health", configFilePath));
         double enemyHealth = Double.parseDouble(getValueFromConfigFile(enemyType + "_attack", configFilePath));
         double playerAttack = Double.parseDouble(getValueFromConfigFile("player_attack", configFilePath));
         double enemyAttack = Double.parseDouble(getValueFromConfigFile(enemyType + "_attack", configFilePath));
+        double swordMod = 0;
+        double shieldMod = 0;
+        double bowMod = 1;
+        double midnightAttackMod = 0;
+        double midnightDefenceMod = 0;
+
+        if (inventory.stream().anyMatch(e -> e instanceof Sword)) { 
+            swordMod = Double.parseDouble(getValueFromConfigFile("sword_attack", configFilePath));;
+        }
+
+        if (inventory.stream().anyMatch(e -> e instanceof Shield)) {
+            shieldMod = Double.parseDouble(getValueFromConfigFile("shield_defence", configFilePath));
+        }
+
+        if (inventory.stream().anyMatch(e -> e instanceof Bow)) {
+            bowMod = 2;
+        }
+
+        if (inventory.stream().anyMatch(e -> e instanceof MidnightArmor)) {
+            midnightAttackMod = Double.parseDouble(getValueFromConfigFile("midnight_armour_attack", configFilePath));
+            midnightDefenceMod = Double.parseDouble(getValueFromConfigFile("midnight_armour_defence", configFilePath));
+        }
 
         for (RoundResponse round : rounds) {
-            assertEquals(-(enemyAttack / 10), round.getDeltaCharacterHealth(), 0.001);
-            assertEquals(-(playerAttack / 5), round.getDeltaEnemyHealth(), 0.001);
+            assertEquals(-((enemyAttack - shieldMod - midnightDefenceMod) / 10), round.getDeltaCharacterHealth(), 0.001);
+            assertEquals(-((bowMod * (playerAttack + swordMod + midnightAttackMod)) / 5), round.getDeltaEnemyHealth(), 0.001);
             enemyHealth += round.getDeltaEnemyHealth();
             playerHealth += round.getDeltaCharacterHealth();
         }
@@ -92,7 +134,7 @@ public class TestBattles {
         DungeonResponse postBattleResponse = genericSpiderSequence(controller,
                 "c_battleTests_basicSpiderPlayerDies");
         BattleResponse battle = postBattleResponse.getBattles().get(0);
-        assertBattleCalculations("spider", battle, false, "c_battleTests_basicSpiderPlayerDies");
+        assertBattleCalculations(getLocalInventory(), "spider", battle, false, "c_battleTests_basicSpiderPlayerDies");
     }
 
     @Test
@@ -101,7 +143,7 @@ public class TestBattles {
         DungeonResponse postBattleResponse = genericSpiderSequence(controller,
                 "c_battleTests_basicSpiderDies");
         BattleResponse battle = postBattleResponse.getBattles().get(0);
-        assertBattleCalculations("spider", battle, true, "c_battleTests_basicSpiderDies");
+        assertBattleCalculations(getLocalInventory(), "spider", battle, true, "c_battleTests_basicSpiderDies");
     }
 
     @Test
@@ -110,7 +152,7 @@ public class TestBattles {
         DungeonResponse postBattleResponse = genericZombieSequence(controller,
                 "c_battleTests_basicZombiePlayerDies");
         BattleResponse battle = postBattleResponse.getBattles().get(0);
-        assertBattleCalculations("zombie", battle, false, "c_battleTests_basicZombiePlayerDies");
+        assertBattleCalculations(getLocalInventory(), "zombie", battle, false, "c_battleTests_basicZombiePlayerDies");
     }
 
     @Test
@@ -120,7 +162,7 @@ public class TestBattles {
         DungeonResponse postBattleResponse = genericZombieSequence(controller,
                 "c_battleTests_basicZombieDies");
         BattleResponse battle = postBattleResponse.getBattles().get(0);
-        assertBattleCalculations("zombie", battle, true, "c_battleTests_basicZombieDies");
+        assertBattleCalculations(inventory, "zombie", battle, true, "c_battleTests_basicZombieDies");
     }
 
     @Test
@@ -130,7 +172,7 @@ public class TestBattles {
         DungeonResponse postBattleResponse = genericHydraSequence(controller,
                 "c_battleTests_basicHydraHydraDies");
         BattleResponse battle = postBattleResponse.getBattles().get(0);
-        assertBattleCalculations("hydra", battle, true, "c_battleTests_basicHydraHydraDies");
+        assertBattleCalculations(getLocalInventory(), "hydra", battle, true, "c_battleTests_basicHydraHydraDies");
     }
 
     @Test
@@ -140,7 +182,7 @@ public class TestBattles {
         DungeonResponse postBattleResponse = genericHydraSequence(controller,
                 "c_battleTests_basicHydraPlayerDies");
         BattleResponse battle = postBattleResponse.getBattles().get(0);
-        assertBattleCalculations("hydra", battle, true, "c_battleTests_basicHydraPlayerDies");
+        assertBattleCalculations(getLocalInventory(), "hydra", battle, true, "c_battleTests_basicHydraPlayerDies");
     }
 
     @Test
@@ -153,10 +195,24 @@ public class TestBattles {
         assertHydraBattleCalculations("hydra", battle, false, "c_battleTests_basicHydraHeal");
     }
 
-    public static void main(String[] args) {
-        TestBattles t = new TestBattles();
-        t.testHealthBelowZeroHydra();
-        t.testHydraHeal();
-        t.testRoundCalculationsHydra();
+
+    private DungeonResponse MercenarySequenceSword(DungeonManiaController controller, String configFile) {
+        DungeonResponse initialResponse = controller.newGame("d_battleTest_MercenarySword", configFile);
+        int mercenaryCount = countEntityOfType(initialResponse, "mercenary");
+
+        assertEquals(1, countEntityOfType(initialResponse, "player"));
+        assertEquals(1, mercenaryCount);
+        return controller.tick(Direction.RIGHT);
+    }
+
+    @Test 
+    public void testWeaponSword() throws IllegalArgumentException, InvalidActionException {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse postBattleResponse = MercenarySequenceSword(controller,"c_battleTests_basicMercenarySword");
+        setInventory(controller.getPlayer().getInventory());
+        assertEquals(true, controller.getPlayer().getInventory().stream().anyMatch(e -> e instanceof Sword));
+        postBattleResponse = controller.tick(Direction.LEFT);
+        BattleResponse battle = postBattleResponse.getBattles().get(0);
+        assertBattleCalculations(controller.getPlayer().getInventory(), "mercenary", battle, true, "c_battleTests_basicMercenarySword");
     }
 }
