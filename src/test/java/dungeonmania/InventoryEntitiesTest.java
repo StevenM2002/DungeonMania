f761605a3c2246e06c434f09d8c947ed6fca624f
Sean Eric Huynh <z5362426@ad.unsw.edu.au>
2022-07-30 22:41:01 +1000

    fixed sceptre duration

--- src/test/java/dungeonmania/InventoryEntitiesTest.java
+++ src/test/java/dungeonmania/InventoryEntitiesTest.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import dungeonmania.CollectibleEntities.MidnightArmour;
 import dungeonmania.MovingEntities.Assassin;
 import dungeonmania.MovingEntities.FriendlyMovement;
 import dungeonmania.MovingEntities.Mercenary;
