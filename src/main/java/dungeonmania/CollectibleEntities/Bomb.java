65e6f9cd91b3e9cb6fc35766c2b4b370a6b564e9
Theo <theoandpebbles@hotmail.com>
2022-07-31 19:30:26 +1000

    persistence now creates a directory to save games to if none exists

--- src/main/java/dungeonmania/CollectibleEntities/Bomb.java
+++ src/main/java/dungeonmania/CollectibleEntities/Bomb.java
@@ -1,14 +1,8 @@
 package dungeonmania.CollectibleEntities;
 
 
-import dungeonmania.Entity;
-import dungeonmania.util.Position;
-
-import java.util.List;
 
 import org.json.JSONObject;
-
-import static dungeonmania.DungeonManiaController.getDmc;
 
 public class Bomb extends InventoryObject implements MapCollectible {
     private static int radius;
